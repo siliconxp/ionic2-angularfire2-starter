c483349294569a2ba7f6e1eda32efb2433084549
Jorge Vergara <javebratt@gmail.com>
2016-11-10 18:07:43 -0500

    Initialize AngularFire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,31 @@ import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+// Import the AF2 Module
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    storageBucket: "",
+    messagingSenderId: ""
+};
+
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+}
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
